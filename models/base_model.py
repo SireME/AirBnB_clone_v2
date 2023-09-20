70bfc38dcfe52083f16edd9dcd548dca26112b71
Mbang Ernest N <mbangernest@yahoo.com>
2023-09-20 13:58:47 +0100

    fix: modify basemodel to be pct compliant

--- models/base_model.py
+++ models/base_model.py
@@ -63,6 +63,6 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
-        if dictionary.get('_sa_instance_state') != None:
+        if dictionary.get('_sa_instance_state'):
             del dictionary['_sa_instance_state']
         return dictionary
