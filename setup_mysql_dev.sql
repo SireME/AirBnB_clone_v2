33247e3191338efe088c5a203e6c309c6e8632fa
Mbang Ernest N <mbangernest@yahoo.com>
2023-09-20 15:16:18 +0100

    setup mysql database

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,18 +1,22 @@
 
 
--- Check if database does not exists and create it --
+-- Check if database does not exists and create it
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- Check if user exists and create one accodingly--
-CREATE USER IF NOT EXISTS 'hbnn_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Check if user exists and create one accodingly
+CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_pwd';
 
--- grant created user all priviledges on the created database --
-USE hbnb_dev_db;
+-- grant created user all priviledges on the created database 
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 
--- grant select privileges on the performance schema--
+-- grant select privileges on the performance schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
---apply changes;
+--apply changes
 FLUSH PRIVILEGES;
+
+-- Set FOREIGN_KEY_CHECKS back to its default value
+SET FOREIGN_KEY_CHECKS=1;
+
+
