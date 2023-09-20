76b6e46ce359174c0e6297cab2d30ca200eb9004
Mbang Ernest N <mbangernest@yahoo.com>
2023-09-20 14:55:33 +0100

    setup mysql database

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,17 @@
+
+
+-- Check if database does not exists and create it --
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Check if user exists and create one accodingly--
+CREATE USER IF NOT EXISTS 'hbnn_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- grant created user all priviledges on the created database --
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+
+-- grant select privileges on the performance schema--
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+--apply changes;
+FLUSH PRIVILEGES;
