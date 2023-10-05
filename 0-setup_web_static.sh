db54ec62ae8434466fab87d44e3f20a164ab8885
Mbang Ernest N <mbangernest@yahoo.com>
2023-10-05 15:36:11 +0100

    fix modify sed and variables

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,41 +2,29 @@
 # This script installs nginx and sets it up for test runs
 
 # update package lsi
-sudo apt update
+sudo apt update -y
 
 # install nginx
 sudo apt install nginx -y
 
 # create folders if they do not exits
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 
 # create fake html file to test nginx
 echo "This is a test: nginx up and runninig" > /data/web_static/releases/test/index.html
 
 # create symbolic link that links current to test
-target=/data/web_static/releases/test/
-link_name=/data/web_static/current
-
-if [ -e "$link_name" ]; then
-	# delete symbolic link if it already exists
-	sudo rm "$link_name"
-fi
-# create symbolic link
-sudo ln -s "$target" "$link_name"
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # give ownership of /data/ folder to ubuntu user and group
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -hR ubuntu:ubuntu /data/
 
 # update nginx to serve content
 update="location /hbnb_static {\
 	alias /data/web_static/current/;\
 	index index.html;\
 }"
-sed -i "/listen 80;/a $update" /etc/nginx/sites-available/default
+sudo sed -i "/listen 80;/a $update" /etc/nginx/sites-available/default
 
 # restart nginx
 sudo service nginx restart
