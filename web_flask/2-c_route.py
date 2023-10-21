434428704c1b4eab3633e2acd85ba18654a6fcdc
Mbang Ernest N <mbangernest@yahoo.com>
2023-10-21 18:37:46 +0100

    add flask route with custom runtime

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This module fires up a flask application on the port
+0.0.0.0 at port 5000 it ensures strict_slashes is false
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_nbnb():
+    """
+    basic flask route on default page
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hello_nbnb_only():
+    """
+    basic flask route on hbnb
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def url_mapping(text):
+    """
+    manipulate urls
+    """
+    my_text = text.replace("_", " ")
+    return f"C {my_text}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
