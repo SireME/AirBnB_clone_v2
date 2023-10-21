07c466bcf4df2e9f81278514c98dca0614a944de
Mbang Ernest N <mbangernest@yahoo.com>
2023-10-21 18:12:29 +0100

    pep8 fixes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,9 +9,14 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route("/")
 def hello_nbnb():
+    """
+    basic flask route on default page
+    """
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
